9476f50367a45b079de88f37f90a96e3a8c1dc64
Jakub Nowicki <kuba@appsilon.com>
2023-11-15 15:55:19 +0100

    Assets for shiny.semantic.

---
+++ R/themes.R
@@ -0,0 +1,23 @@
+#' Supported semantic themes
+#' @return A character vector of supported themes
+#'
+#' @export
+SUPPORTED_THEMES <- c(
+  "cerulean",
+  "cosmo",
+  "cyborg",
+  "darkly",
+  "flatly",
+  "journal",
+  "lumen",
+  "paper",
+  "readable",
+  "sandstone",
+  "simplex",
+  "slate",
+  "solar",
+  "spacelab",
+  "superhero",
+  "united",
+  "yeti"
+)
